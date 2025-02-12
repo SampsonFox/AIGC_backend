47c67197be4c81d570c9a938d8b416429f143bdd
SampsonFox <sanpsonfox@gmail.com>
2025-02-12 20:11:39 +0800

    Django脚手架

--- start_backend.sh
+++ start_backend.sh
@@ -2,7 +2,8 @@
 #gnome-terminal -t "pypy3" -x bash -c "pypy3;exec bash;"
 {
 	echo "Lunching python"
-	python
+	cd /codes/AIGC_backend/AIGC_backend/aigcBackend
+	python manage.py runserver 0.0.0.0:8000
 }
 
 wait
